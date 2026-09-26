16854513b6f6db5781b4a561da9d1d81833769aa
artyomkinematica <arderesh@yandex.ru>
2026-09-26 21:05:25 +0300

    main + calculator (полный)

--- src/toolkit/errors.py
+++ src/toolkit/errors.py
@@ -5,35 +5,26 @@ class ToolkitError(Exception):
 class CalcError(ToolkitError):
     """ошибки кальулятора"""
 
-###
 class EmptyExpressionError(CalcError):
     pass
-###
 
-###
 class InvalidTokenError(CalcError):
     pass
-###
 
 class InvalidExpressionError(CalcError):
     pass
 
-###
 class DivisionByZeroError(CalcError):
     pass
-###
 
 class ConvError(ToolkitError):
     """ошибки конвертора"""
 
-
 class UnknownUnitError(ConvError):
     pass
 
-
 class IncompatibleUnitsError(ConvError):
     pass
-
 
 class BelowAbsoluteZeroError(ConvError):
     pass
