e610eab2ef56ee678021fc4e1df5a6364d9e0ce0
artyomkinematica <arderesh@yandex.ru>
2026-09-26 22:14:30 +0300

    converter + tests to converter

--- src/toolkit/__main__.py
+++ src/toolkit/__main__.py
@@ -3,6 +3,7 @@ import sys
 
 from toolkit.calculator import calculate
 from toolkit.errors import ToolkitError
+from toolkit.converter import convert
 
 
 def format_result(value):
@@ -21,6 +22,11 @@ def build_parser():
     calc_parser = subparsers.add_parser('calc', help='вычислить арифметическое выражение')
     calc_parser.add_argument('expression', help='выражение в кавычках, например "2+3.1*2"')
 
+    convert_parser = subparsers.add_parser('convert', help='конвертер величин')
+    convert_parser.add_argument('value', type=float, help='число')
+    convert_parser.add_argument('--from', dest='from_unit', required=True, metavar='UNIT', help='из какой единицы переводить')
+    convert_parser.add_argument('--to', dest='to_unit', required=True, metavar='UNIT', help='то, в какую единицу следует перевести исходную')
+
     return parser
 
 
@@ -36,7 +42,7 @@ def main(argv=None):
         if args.command == 'calc':
             result = calculate(args.expression)
         else:
-            pass
+            result = convert(args.value, args.from_unit, args.to_unit)
     except ToolkitError as error:
         print(error, file=sys.stderr)
         return 2
--- src/toolkit/converter.py
+++ src/toolkit/converter.py
@@ -1,1 +1,80 @@
+from toolkit.errors import BelowAbsoluteZeroError, InvalidValueError, UnknownUnitError, IncompatibleUnitsError
 
+LENG = {
+    "mm": 0.001,
+    "cm": 0.01,
+    "dm": 0.1,
+    "m": 1.0,
+    "km": 1000.0
+}
+
+MASSA = {
+    "mg": 0.001,
+    "g": 1.0,
+    "kg": 1000.0
+}
+
+TEMP = ('c', 'f', 'k')
+
+ABS_ZERO = {
+    'c': -273.15,
+    'f': -459.67,
+    'k': 0
+}
+
+
+def group(unit):
+    if unit in LENG:
+        return "length"
+    elif unit in MASSA:
+        return "mass"
+    elif unit in TEMP:
+        return "temperature"
+    raise UnknownUnitError('Неизвестная единица')
+
+
+def to_c(unit, value):
+    if unit == 'c':
+        return value
+    elif unit == 'f':
+        return (value - 32) * 5 / 9
+    elif unit == 'k':
+        return value - 273.15
+
+
+def from_c(unit, value):
+    if unit == 'c':
+        return value
+    if unit == 'f':
+        return value * 9 / 5 + 32
+    elif unit == 'k':
+        return value + 273.15
+
+
+def convert(value, from_unit, to_unit):
+    if not isinstance(value, (int, float)):
+        raise InvalidValueError('Значение должно быть числом')
+
+    from_unit = from_unit.lower()
+    to_unit = to_unit.lower()
+
+    group_from = group(from_unit)
+    group_to = group(to_unit)
+
+    if group_from != group_to:
+        raise IncompatibleUnitsError('Группы должны быть одинаковыми')
+
+    if group_from == 'temperature':
+        if value < ABS_ZERO[from_unit]:
+            raise BelowAbsoluteZeroError('Значение температуры ниже абсолютного нуля')
+        cels = to_c(from_unit, value)
+        return float(from_c(to_unit, cels))
+
+    if group_from == 'length':
+        gr = LENG
+    else:
+        gr = MASSA
+
+    base_unit = value * gr[from_unit]
+
+    return float(base_unit / gr[to_unit])
